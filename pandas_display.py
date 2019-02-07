7e882abb86bf1bc6cd4acb698c285de897ecb105
ardunn <ardunn@lbl.gov>
2019-02-07 13:52:13 -0800

    add pandas display snippet

---
+++ pandas_display.py
@@ -0,0 +1,11 @@
+"""
+Expand pandas output
+
+Last update - 02.05.2019
+"""
+
+import pandas as pd
+# pd.set_option('display.height', 1000)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
