3c6e1bec8bb39717f0c8d52ca26ec59a450f4fca
ardunn <ardunn@lbl.gov>
2019-12-25 01:56:32 -0800

    add youtube dl

---
+++ download_youtube_audio.sh
@@ -0,0 +1,8 @@
+# Downloading youtube audio with youtube-dl
+
+# first pip install youtube-dl
+
+
+
+# then use
+youtube-dl --extract-audio --audio-format mp3 --no-check-certificate [URL]
---
+++ view_time_machine_logs_OSXMojave.sh
@@ -0,0 +1,2 @@
+printf '\e[3J' && log show --predicate 'subsystem == "com.apple.TimeMachine"' --info --last 6h | grep -F 'eMac' | grep -Fv 'etat' | awk -F']' '{print substr($0,1,19), $NF}' 
+
