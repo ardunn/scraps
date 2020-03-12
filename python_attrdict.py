fdb52654bfab85c2206f1ccc4e7a59f1de644ad4
Alex Dunn <denhaus@gmail.com>
2020-03-12 00:35:08 -0700

    add some docs for attrdict

--- python_attrdict.py
+++ python_attrdict.py
@@ -1,3 +1,22 @@
+'''
+A class for accessing dictionaries as if they were classes (syntactically nice)
+
+You can't access attributes outside of what you declare in the init
+
+
+For example:
+
+tasks = {"a": [1, 2], "b": [10, 11]}
+task_nice = AttrDict(tasks)
+
+
+tasks.a.append(3)
+tasks["b"].append(12)
+
+tasks.c # should throw keyerror or attributerror
+'''
+
+
 class AttrDict(dict):
     """ Syntax candy """
     __getattr__ = dict.__getitem__
